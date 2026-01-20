f06056dbf83646a211f6bff7ff807614aa4c4b90
Anuj Kumar <anujkumar@Anujs-Mac-mini.local>
2026-01-20 23:28:08 +0530

    chore: add sql file for manual category insertion

---
+++ scripts/categories.sql
@@ -0,0 +1,5 @@
+INSERT INTO categories (name, slug, description, image_url, status, display_order) VALUES 
+('Website', 'website', '', '', 'active', 0),
+('Plugins', 'plugins', '', '', 'active', 0),
+('Addons', 'addons', '', '', 'active', 0),
+('Templates', 'templates', '', '', 'active', 0);

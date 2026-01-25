2db94fce591df5c248c0e175d52205a284d948b1
Anuj Kumar <anujkumar@Anujs-Mac-mini.local>
2026-01-25 19:29:05 +0530

    Fix debug script dependencies

--- debug_images.php
+++ debug_images.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/config/database.php';
+require_once __DIR__ . '/includes/functions.php';
 require_once __DIR__ . '/classes/Database.php';
 
 ini_set('display_errors', 1);
